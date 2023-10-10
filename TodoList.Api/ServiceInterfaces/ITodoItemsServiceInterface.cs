eed06a4a9aaeb2c218beeb6e7895ef4039285dbf
ckundudas <147563013+ckundudas@users.noreply.github.com>
2023-10-11 10:09:16 +1100

    Add files via upload

---
+++ TodoList.Api/ServiceInterfaces/ITodoItemsServiceInterface.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoList.Api.ServiceInterfaces
+{
+    public interface ITodoItemsServiceInterface
+    {
+        Task<List<TodoItem>> GetTodoItems();
+        ValueTask<TodoItem> GetTodoItem(Guid id);
+        Task<IActionResult> PutTodoItem(Guid id, TodoItem todoItem);
+    }
+}
